383a542be874d2abfc40b3938f912dda46845f5a
Bere_ti0l <Bere_ti0l@9-23>
2023-05-05 15:10:51 +0300

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inp num ");
+        }
+
+        static int[] InitMass()
+        {
+            Random r = new Random();
+            int[] mass = new int[10];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = r.Next(-50, 50);
+            }
+            return mass;
+        }
+        static int Check(int[] mass, int user)
+        {
+            int count = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] < user)
+                    count++;
+            }
+            return count;
+        }
+
+    }
+
+    
+
+
+
+
+}
